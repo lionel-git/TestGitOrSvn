c6703d7467edd6636a4e7996c808c55e555d3a8f
Lionel Desorme <lionel.desorme@free.fr>
2019-05-25 16:45:07 +0200

    Initial commit

---
+++ TestGitOrSvn/CheckDirectory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitOrSvn
+{
+    public enum DirectoryType { None, Svn, Git };
+
+    public class CheckDirectory
+    {
+        public static DirectoryType GetDirectoryType(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return DirectoryType.None;
+            else if (Directory.Exists(Path.Combine(path, ".svn")))
+                return DirectoryType.Svn;
+            else if(Directory.Exists(Path.Combine(path, ".git")))
+                return DirectoryType.Git;
+            var directoryInfo = new DirectoryInfo(path);
+            return GetDirectoryType(directoryInfo.Parent?.FullName);
+        }
+    }
+}
---
+++ TestGitOrSvn/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitOrSvn
+{
+    class Program
+    {
+
+        static List<string>_testDir = new List<string>() { @"..", @".", @"c:\tmp\diff", @"D:\my_projects\Solitaire\Solitaire", @"D:\svn\MyAlarm\MyAlarm" };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                foreach (var dir in _testDir)
+                {
+                    string absolute = Path.GetFullPath(dir);
+                    Console.WriteLine($"{dir} {absolute} => {CheckDirectory.GetDirectoryType(absolute)}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
